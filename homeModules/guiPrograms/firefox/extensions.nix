ddd517dd871d2ca1df3ed4b7adb64e1a00f0ba6b
peace <peace@archlinux>
2024-10-31 20:39:15 +0530

    added firefox-color and stylus

--- homeModules/guiPrograms/firefox/extensions.nix
+++ homeModules/guiPrograms/firefox/extensions.nix
@@ -6,8 +6,10 @@
 
 {
   programs.firefox.profiles.default.extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+    firefox-color
     proton-pass
     proton-vpn
+    stylus
     ublock-origin
   ];
 }
